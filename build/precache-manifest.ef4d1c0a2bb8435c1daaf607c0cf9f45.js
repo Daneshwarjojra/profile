d2227f62067c99eeeaaddcf42aee2f79a26aa447
Sunny <daneshwar@babatrucks.com>
2020-05-31 13:08:15 +0530

    build

---
+++ build/precache-manifest.ef4d1c0a2bb8435c1daaf607c0cf9f45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0a2ab1d28a4d8b9b237bed71fb78d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90aa802c63392e8aa5ea",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "38b28de12189d7b3e46c",
+    "url": "/static/css/main.a6f30184.chunk.css"
+  },
+  {
+    "revision": "90aa802c63392e8aa5ea",
+    "url": "/static/js/2.5a4e67ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a4e67ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38b28de12189d7b3e46c",
+    "url": "/static/js/main.5ceb1355.chunk.js"
+  },
+  {
+    "revision": "b67166582017c4b6af5f",
+    "url": "/static/js/runtime-main.da2c29c0.js"
+  },
+  {
+    "revision": "035d067f78eca9a048bb06275b5760db",
+    "url": "/static/media/citysprite.035d067f.png"
+  }
+]);
